9c1b9d2ae2a6f7a1368c3aa997452c810c0044f6
michael-172 <85269483+michael-172@users.noreply.github.com>
2022-11-12 23:59:03 +0200

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ const Projects = () => (
             </TagList>
           </div>
           <UtilityList>
-            <ExternalLinks href={visit}>Visit</ExternalLinks>
+            <ExternalLinks href={visit}></ExternalLinks>
             <ExternalLinks href={source}>Source</ExternalLinks>
           </UtilityList>
         </BlogCard>
